ef311765adefc88d3078e26e4ec304c6b6aa2a36
zzoxyyy <90214542+zzoxyyy@users.noreply.github.com>
2022-12-25 23:37:46 +0100

    Add files via upload

---
+++ Homework 3/szj/src/test/java/finki/dians/szj/SzjApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.dians.szj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
